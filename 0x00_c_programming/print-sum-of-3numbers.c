a05ed9df7e729a30e89a312571a561b1e1a3eb7e
Eliz-Lea <elizabethlea6@gmail.com>
2023-02-24 19:21:03 +0000

    floating sum

---
+++ 0x00_c_programming/print-sum-of-3numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  double y=5.0;
+   int x=3;
+   float z=2.8;
+    float sum;
+        //print sum of three numbers
+        printf("The sum of y, x, and z is %f", sum);
+
+     return 0;
+}
